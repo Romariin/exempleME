44187c8f5721edf0c307a35b78a563da53ffd988
Romariin <romainrogier2@gmail.com>
2022-03-04 16:38:23 +0100

    :wrench: Add schema to routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var mongoose = require("mongoose");
 var router = express.Router();
+
+const UserSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+});
+
+var User = mongoose.model("User", UserSchema, "users");
+
 
 /* GET users listing. */
 router.get('/add', function(req, res) {
-
+  User.create({
+    name: 'John',
+    email: 'Salam',
+    password: '123'
+  }).then(function(user){
+    res.send(user);
+  });
 });
 
 router.get('/get', function(req, res) {
