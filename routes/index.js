4b0b3bbc5f2e4ed8d914d849e1758a1bd7e404a7
Romariin <romainrogier2@gmail.com>
2022-03-10 16:26:43 +0100

    :zap: Import verification on routes (isAuth/needAuth/isAdmin)

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,49 @@
 var express = require('express');
 var router = express.Router();
-const verifRegister = require('../public/javascript/verifRegister');
-const verifLogin = require('../public/javascript/verifLogin');
-const authenticateJWT = require('../middleware/authenticateJWT');
+const {verifRegister, verifLogin, isAdmin, isAuth, needAuth} = require('../middleware/auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.cookies.access_token });
+
+router.get('/', isAuth, function (req, res, next) {
+    const test = res.header('authorization');
+    console.log(test)
+    res.render('index', {title: 'Express', user: req.user});
 });
 
-router.get('/register', function(req, res, next) {
-  if (req.cookies.access_token) return res.redirect("/")
-  res.render('register', { error: '', user: req.cookies.access_token});
+router.get('/register', isAuth, function (req, res, next) {
+    if (req.user) return res.redirect("/")
+    res.render('register', {error: '', user: req.user});
 });
 
-router.post('/register', function(req, res, next) {
-  verifRegister(req, res)
+router.post('/register', verifRegister, function (req, res, next) {
 });
 
-router.get('/login', function(req, res, next) {
-  if (req.cookies.access_token) return res.redirect("/")
-  res.render('login', { error: '', user: req.cookies.access_token });
+router.get('/login', isAuth, function (req, res, next) {
+    if (req.user) return res.redirect("/")
+    res.render('login', {error: '', user: req.user});
+
 });
 
-router.post('/login', function(req, res) {
-  verifLogin(req, res)
+router.post('/login', verifLogin, function (req, res) {
+    res.send(res.locals);
 });
 
-router.get('/logout', function(req, res){
-  if (!req.cookies.access_token) return res.redirect("/");
-  req.flash("success", "Vous avez été correctement déconnecter!")
-  return res
-      .clearCookie("access_token")
-      .status(200)
-      .redirect("/")
+router.get('/logout', function (req, res) {
+    req.flash("success", "Vous avez été correctement déconnecter!")
+    return res
+        .clearCookie("access_token")
+        .status(200)
+        .redirect("/")
 })
 
-router.get('/profile', authenticateJWT, function(req, res){
-  console.log(req.user);
-  res.render('index', { title: 'Profile', user: req.cookies.access_token });
+router.get('/profile', needAuth, function (req, res) {
+    const test = res.header('x-access-token')
+    console.log(test)
+    res.render('index', {title: 'Profile', user: req.user});
+})
+
+router.get('/admin', needAuth, isAdmin, function (req, res) {
+    res.render('index', {title: 'Admin', user: req.user});
 })
 
 
