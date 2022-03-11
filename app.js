e7c4bc9c4b2b19778809d74c09ccaaca93f79644
Romariin <romainrogier2@gmail.com>
2022-03-11 10:26:17 +0100

    :closed_lock_with_key: Add cors and toggle on urlencoded

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cors = require('cors');
 var flash = require('express-flash')
 
 var session = require('express-session');
@@ -23,7 +24,7 @@ app.use(session({
 app.use(flash());
 
 // Configure More Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // view engine setup
@@ -31,8 +32,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
