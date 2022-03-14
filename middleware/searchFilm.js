9e6cc12bb8f1973ab036cbff400f777b71f21caa
Romariin <romainrogier2@gmail.com>
2022-03-14 18:49:35 +0100

    :zap: Improve searchFilm (getGenre, fetchNewFilm)

--- middleware/searchFilm.js
+++ middleware/searchFilm.js
@@ -1,5 +1,22 @@
 // Récuperer les films via l'API TMDB
 const fetch = require("node-fetch");
+
+const fetchNewFilm = async (req, res, next) => {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY_TMDB}&language=${process.env.API_TMDB_LANG}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  req.newFilm = data;
+  next();
+};
+
+const fetchFilmById = async (req, res, next) => {
+  const url = `https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.API_KEY_TMDB}&language=${process.env.API_TMDB_LANG}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  req.filmById = data;
+  next();
+};
+
 const fetchFilmByName = async (req, res, next) => {
   const filmName = req.body.film;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY_TMDB}&language=${process.env.API_TMDB_LANG}&query=${filmName}`;
@@ -9,6 +26,16 @@ const fetchFilmByName = async (req, res, next) => {
   next();
 };
 
+const getGenre = async (req, res, next) => {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY_TMDB}&language=${process.env.API_TMDB_LANG}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  req.genre = data;
+  next();
+};
+
 module.exports = {
   fetchFilmByName,
+  getGenre,
+  fetchNewFilm,
 };
